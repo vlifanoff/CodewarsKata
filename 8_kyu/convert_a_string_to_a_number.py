81acbcee7f6293795b8de408287ba1df6c96f2ba
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 22:11:10 +0300

    Convert a String to a Number!
    https://www.codewars.com/kata/544675c6f971f7399a000e79

---
+++ 8_kyu/convert_a_string_to_a_number.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Convert a String to a Number!: https://www.codewars.com/kata/544675c6f971f7399a000e79"""
+
+
+def string_to_number(s):
+    return int(s)
