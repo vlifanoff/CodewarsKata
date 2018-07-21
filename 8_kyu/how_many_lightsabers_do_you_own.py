74ce1767838e65a44f1f47bd1c466a522d76a06c
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 22:07:54 +0300

    How many lightsabers do you own?
    https://www.codewars.com/kata/51f9d93b4095e0a7200001b8

---
+++ 8_kyu/how_many_lightsabers_do_you_own.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""How many lightsabers do you own?: https://www.codewars.com/kata/51f9d93b4095e0a7200001b8"""
+
+
+def howManyLightsabersDoYouOwn(s=''):
+    return 18 if s == "Zach" else 0
