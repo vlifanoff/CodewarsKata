9ca6f87e1755b39e4c3d9134e374cc3af26cbdd6
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 21:57:21 +0300

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe
    https://www.codewars.com/kata/56efc695740d30f963000557

---
+++ 8_kyu/altERnaTIng_cAsE__ALTerNAtiNG_CaSe.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""altERnaTIng cAsE <=> ALTerNAtiNG CaSe: https://www.codewars.com/kata/56efc695740d30f963000557"""
+
+
+def to_alternating_case(s):
+    return s.swapcase()
