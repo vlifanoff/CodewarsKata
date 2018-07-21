8a7518a1bc155e4bba00dde11122888009ae44ba
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 23:03:27 +0300

    Digitize
    https://www.codewars.com/kata/5417423f9e2e6c2f040002ae

---
+++ 7_kyu/digitize.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Digitize: https://www.codewars.com/kata/5417423f9e2e6c2f040002ae"""
+
+
+def digitize(n):
+    return list(map(int, list(str(n))))
