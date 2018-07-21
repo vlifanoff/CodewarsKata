3bd6b36f4d30016ef8cc80a7b304a5a57e30bb71
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 21:32:00 +0300

    Even or Odd
    https://www.codewars.com/kata/53da3dbb4a5168369a0000fe

---
+++ 8_kyu/even_or_odd.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Even or Odd: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe"""
+
+
+def even_or_odd(number):
+    return "Even" if number % 2 == 0 else "Odd"
