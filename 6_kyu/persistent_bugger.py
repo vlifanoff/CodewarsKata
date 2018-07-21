0ab28b2d9928b2076d2f2386d22fcd618f821084
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 23:43:48 +0300

    Persistent Bugger. Version 2
    https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec

--- 6_kyu/persistent_bugger.py
+++ 6_kyu/persistent_bugger.py
@@ -4,12 +4,13 @@
 """Persistent Bugger: https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec"""
 
 from functools import reduce
+from operator import mul
 
 
 def persistence(n):
     counter = 0
     while n > 9:
-        n = reduce(lambda x, y: x * y, map(int, str(n)))
+        n = reduce(mul, map(int, str(n)))
         counter += 1
 
     return counter
