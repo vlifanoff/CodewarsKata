395b9a91cd0b843c7aa5d5990d95b9f66054693f
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 22:05:52 +0300

    Basic Training: Add item to an Array
    https://www.codewars.com/kata/511f0fe64ae8683297000001

---
+++ 8_kyu/basic_training_add_item_to_an_array.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Basic Training: Add item to an Array: https://www.codewars.com/kata/511f0fe64ae8683297000001"""
+
+websites = list()
+websites.append("codewars")
