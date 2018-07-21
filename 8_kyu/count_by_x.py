8a885f32e5703231f51855b1a576659ef7823414
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 21:47:39 +0300

    Count by X
    https://www.codewars.com/kata/5513795bd3fafb56c200049e

---
+++ 8_kyu/count_by_x.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Count by X: https://www.codewars.com/kata/5513795bd3fafb56c200049e"""
+
+
+def count_by(x, n):
+    return range(x, x * n + 1, x)
