74fb50b42a75f49e4ed27d054670cdc795664fe8
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 21:43:46 +0300

    String repeat
    https://www.codewars.com/kata/57a0e5c372292dd76d000d7e

---
+++ 8_kyu/string_repeat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""String repeat: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e"""
+
+
+def repeat_str(repeat, string):
+    return string * repeat
