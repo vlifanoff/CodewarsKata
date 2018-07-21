f234f005cb75c7f8ac7cb03b40b47cb92a976372
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 22:09:49 +0300

    Remove exclamation marks
    https://www.codewars.com/kata/57a0885cbb9944e24c00008e

---
+++ 8_kyu/remove_exclamation_marks.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Remove exclamation marks: https://www.codewars.com/kata/57a0885cbb9944e24c00008e"""
+
+
+def remove_exclamation_marks(s):
+    return s.replace('!', '')
