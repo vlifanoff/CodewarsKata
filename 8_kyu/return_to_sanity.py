7efe7f5be1cf5da834d8678df40f19e9218ded05
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 22:01:29 +0300

    Return to Sanity
    https://www.codewars.com/kata/514a7ac1a33775cbb500001e

---
+++ 8_kyu/return_to_sanity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Return to Sanity: https://www.codewars.com/kata/514a7ac1a33775cbb500001e"""
+
+
+def mystery():
+    results = {'sanity': 'Hello'}
+    return results
