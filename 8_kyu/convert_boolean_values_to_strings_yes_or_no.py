27b7e68f12ef4da83f3b34c847e4b16a4aa0fca1
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 21:48:58 +0300

    Convert boolean values to strings 'Yes' or 'No'
    https://www.codewars.com/kata/53369039d7ab3ac506000467

---
+++ 8_kyu/convert_boolean_values_to_strings_yes_or_no.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Convert boolean values to strings 'Yes' or 'No': https://www.codewars.com/kata/53369039d7ab3ac506000467"""
+
+
+def bool_to_word(b):
+    return 'Yes' if b else 'No'
