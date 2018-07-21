b2d1dcede7e96d3d009f543afb22a263cc7cf817
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 20:42:25 +0300

    Return Negative
    https://www.codewars.com/kata/55685cd7ad70877c23000102

---
+++ 8_kyu/return_negative.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def make_negative(number):
+    """Return Negative: https://www.codewars.com/kata/55685cd7ad70877c23000102"""
+    return number if number <= 0 else -number
