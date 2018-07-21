720e0bcfd624b52ba352c69c8eec210ed6454bae
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 23:39:54 +0300

    Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!! Version 2
    https://www.codewars.com/kata/5626b561280a42ecc50000d1

--- 6_kyu/take_a_number_and_sum_its_digits_raised_to_the_consecutive_powers_and__eureka.py
+++ 6_kyu/take_a_number_and_sum_its_digits_raised_to_the_consecutive_powers_and__eureka.py
@@ -6,4 +6,4 @@ https://www.codewars.com/kata/5626b561280a42ecc50000d1"""
 
 
 def sum_dig_pow(a, b):
-    return filter(lambda x: x == sum(int(str(x)[i]) ** (i + 1) for i in range(len(str(x)))), range(a, b + 1))
+    return filter(lambda x: x == sum(int(n) ** (i + 1) for i, n in enumerate(str(x))), range(a, b + 1))
