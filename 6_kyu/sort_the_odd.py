a49ab57d46472244a8175edd174a7024d21607b9
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 23:19:19 +0300

    Sort the odd
    https://www.codewars.com/kata/578aa45ee9fd15ff4600090d

---
+++ 6_kyu/sort_the_odd.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Sort the odd: https://www.codewars.com/kata/578aa45ee9fd15ff4600090d"""
+
+
+def sort_array(source_array):
+    is_odd = lambda x: x & 0b1
+    tmp = sorted(filter(is_odd, source_array), reverse=True)
+    return [tmp.pop() if is_odd(source_array[i]) else source_array[i] for i in range(len(source_array))]
