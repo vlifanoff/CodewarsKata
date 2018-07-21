3ac83b0b8f9dc50f41c1b9dea06b3b841855fb50
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 21:41:24 +0300

    Double Char
    https://www.codewars.com/kata/56b1f01c247c01db92000076

---
+++ 8_kyu/double_char.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Double Char: https://www.codewars.com/kata/56b1f01c247c01db92000076"""
+
+
+def double_char(s):
+    return str().join([x * 2 for x in s])
