393d9ed7eb8983f2fe6ed469b3f39fa66d466f3f
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 21:40:27 +0300

    Printing Array elements with Comma delimiters
    https://www.codewars.com/kata/56e2f59fb2ed128081001328

---
+++ 8_kyu/printing_array_elements_with_comma_delimiters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Printing Array elements with Comma delimiters: https://www.codewars.com/kata/56e2f59fb2ed128081001328"""
+
+
+def print_array(arr):
+    return ','.join(map(str, arr))
