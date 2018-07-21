ae953f7b5a50a45960297e7ae18e1905ddf54e00
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 21:22:29 +0300

    Keep up the hoop
    https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145

---
+++ 8_kyu/keep_up_the_hoop.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Keep up the hoop: https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145"""
+
+
+def hoopCount(n):
+    return "Keep at it until you get it" if n < 10 else "Great, now move on to tricks"
