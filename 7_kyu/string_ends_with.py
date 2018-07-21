abd0c9eed1f4016320adbd1455b9d228050a7f7f
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 22:57:10 +0300

    String ends with?
    https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d

---
+++ 7_kyu/string_ends_with.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""String ends with?: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d"""
+
+
+def solution(string, ending):
+    return string.endswith(ending)
