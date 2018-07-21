10142e628ae860a44c2073ef79fc18ac083b1864
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-07-21 22:56:35 +0300

    Return substring instance count
    https://www.codewars.com/kata/5168b125faced29f66000005

---
+++ 7_kyu/return_substring_instance_count.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Return substring instance count: https://www.codewars.com/kata/5168b125faced29f66000005"""
+
+
+def solution(full_text, search_text):
+    return full_text.count(search_text)
